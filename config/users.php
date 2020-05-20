e760b906f16ccecc6a0584847fe56cd243189165
thanhch <thanhch@gamota.com>
2020-05-20 10:19:20 +0700

    tao file users.php config

---
+++ config/users.php
@@ -0,0 +1,9 @@
+<?php
+// /config/users.php file contents
+$config = [
+    'Users.Social.login' => true,
+    'OAuth.providers.google.options.clientId' => '18121143362-rav4cft40j8c46unv0314ndeoavoa7g6.apps.googleusercontent.com',
+    'OAuth.providers.google.options.clientSecret' => '2Bo5OEEtAU_Lc1lLKFwzbfYa',
+];
+
+return $config;
